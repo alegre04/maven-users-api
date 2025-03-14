bffc0f69ec8095cbd075720950b91cbe59271d2d
aldo2510 <josealdotrucios@gmail.com>
2025-03-14 18:36:42 -0500

    Update MiAplicacionJavaApplication.java

--- src/main/java/com/entrenamiento/certero/MiAplicacionJavaApplication.java
+++ src/main/java/com/entrenamiento/certero/MiAplicacionJavaApplication.java
@@ -9,4 +9,11 @@ public class MiAplicacionJavaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MiAplicacionJavaApplication.class, args);
     }
+    @Override
+    public void run(String... args) throws Exception {
+        // Poblamos la base de datos con usuarios predeterminados
+        userRepository.save(new User(null, "Juan Pérez", "juan.perez@correo.com"));
+        userRepository.save(new User(null, "Ana Gómez", "ana.gomez@correo.com"));
+        userRepository.save(new User(null, "Carlos Rodríguez", "carlos.rodriguez@correo.com"));
+    }
 }
